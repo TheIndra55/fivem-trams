aaedd8d4535694e867bca7698079ff3f788a48c4
Indra <wuppie55@gmail.com>
2021-12-08 21:18:59 +0100

    Refactor and replace door code

--- client.lua
+++ client.lua
@@ -6,7 +6,7 @@ local currentNode = nil
 
 -- list of stations and their node ids
 -- you probably don't have to modify this except when you are renaming stations
-local stations = {
+local stations <const> = {
     { node = 179,  name = "Strawberry",      },
     { node = 271,  name = "Puerto Del Sol",  },
     { node = 388,  name = "LSIA Parking",    },
@@ -31,20 +31,22 @@ local stations = {
     { node = 2246, name = "Strawberry"       }
 }
 
-local doors = {left = {0, 2}, right = {1, 3}}
+-- don't edit this, see readme for configuration
+local defaultStationText <const> = "You are on line ~BLIP_536~ the next station is ~g~~a~"
 
 Citizen.CreateThread(function()
-    -- if enabled this will tell gta to spawn trains naturally
-    if config.enableTrains or config.enableTrams then
-        if config.enableTrains then SwitchTrainTrack(0, true) end
+    -- this will tell gta to spawn trains naturally
+	SwitchTrainTrack(0, true)
+	SwitchTrainTrack(3, true)
 
-        if config.enableTrams then SwitchTrainTrack(3, true) end
+	SetTrainTrackSpawnFrequency(0, 120000) -- found by Disquse
+	SetRandomTrains(1)
 
-        SetTrainTrackSpawnFrequency(0, 120000) -- found by Disquse
-        SetRandomTrains(1)
-    end
+	-- disable doors opening
+	SetTrainsForceDoorsOpen(false)
 
-    AddTextEntry("NEXT_STATION_NOTIFICATION", config.text)
+	local text = GetConvar("trams_station_text", defaultStationText)
+    AddTextEntry("NEXT_STATION_NOTIFICATION", text)
 end)
 
 -- fetch list of trains every sec to improve performance
@@ -73,16 +75,6 @@ CreateThread(function()
     end
 end)
 
-function drawThere(coords, text)
-    SetDrawOrigin(coords)
-
-    BeginTextCommandDisplayText("STRING")
-    AddTextComponentSubstringPlayerName(text)
-    EndTextCommandDisplayText(0.0, 0.0)
-
-    ClearDrawOrigin()
-end
-
 CreateThread(function()
     while true do
         Wait(0)
@@ -105,45 +97,6 @@ CreateThread(function()
         end
     end
 end)
-
-CreateThread(function()
-    if not config.closeDoors then return end
-
-    while true do
-        Wait(0)
-
-        -- train doors in gta automatically open when closed so set it closed every tick if driving
-        for _, train in pairs(trains) do
-            -- only do for close trains
-            if train[2] < 200 then
-                if train[3] --[[ speed ]] > 0.5 then
-                    SetVehicleDoorsShut(train[1], true)
-                elseif config.closeDoorsAtStations then
-                    -- trams in gta are two "metrotrains" which both face another direction so doors are at other side
-                    -- if the current train entity has no second carriage we can assume it's the back train etc
-                    if DoesEntityExist(GetTrainCarriage(train[1], 1)) then
-                        -- close right
-                        -- drawThere(GetEntityCoords(train[1]), "right")
-                        shutSide(train[1], "right")
-                    else
-                        -- drawThere(GetEntityCoords(train[1]), "left")
-                        shutSide(train[1], "left")
-                    end
-                end
-            end
-        end
-
-        -- DrawBox(GetEntityCoords(trains[1][1]) - 1, GetEntityCoords(trains[1][1]) + 1, 255, 255, 255, 100)
-    end
-end)
-
-function shutSide(vehicle, side)
-    if doors[side] then
-        for _, door in pairs(doors[side]) do
-            SetVehicleDoorShut(vehicle, door, true)
-        end
-    end
-end
 
 function compareCoords(a, b) return a[2] < b[2] end
 
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,11 @@
 fx_version "adamant"
 game "gta5"
 
-author 'TheIndra'
+author "TheIndra"
+description "Add trains and trams to your server!"
+version '1.0.2'
+
+lua54 "yes"
 
 client_scripts{
     "config.lua",
